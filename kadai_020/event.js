5e926eb2688f8780e4de04165af44697d4702bc2
sou71512 <143829679+sou71512@users.noreply.github.com>
2023-10-23 20:10:07 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-const pushBtn = document.getElementById('btn');
-
-const btnText = document.getElementById('text');
-
-pushBtn.addEventListener('click', () => {
-    btnText.style = 'none'
-
-    btnText.textContent = 'ボタンをクリックしました'
-})
